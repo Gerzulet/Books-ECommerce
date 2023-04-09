6960e52dc7cc641d1660d6ad3d30edebf2acb21a
Gerzulet <german.alejandrozulet@gmail.com>
2023-04-09 19:44:02 -0300

    [ADD] Favicon Icon

--- src/index.js
+++ src/index.js
@@ -5,40 +5,40 @@ import App from './App';
 
 
 
- import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 
-  // TODO: Add SDKs for Firebase products that you want to use
+// TODO: Add SDKs for Firebase products that you want to use
 
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-
-  // Your web app's Firebase configuration
-
-  const firebaseConfig = {
-
-    apiKey: "AIzaSyBJzS51SIOwWTTuFpIBs7y-ocbAhHX3yQA",
-
-    authDomain: "test-app-1bfde.firebaseapp.com",
-
-    projectId: "test-app-1bfde",
-
-    storageBucket: "test-app-1bfde.appspot.com",
-
-    messagingSenderId: "462147695358",
-
-    appId: "1:462147695358:web:e6354417a5976f86366baf"
-
-  };
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 
- const app =  initializeApp(firebaseConfig);
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBJzS51SIOwWTTuFpIBs7y-ocbAhHX3yQA",
+
+  authDomain: "test-app-1bfde.firebaseapp.com",
+
+  projectId: "test-app-1bfde",
+
+  storageBucket: "test-app-1bfde.appspot.com",
+
+  messagingSenderId: "462147695358",
+
+  appId: "1:462147695358:web:e6354417a5976f86366baf"
+
+};
+
+
+initializeApp(firebaseConfig);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
-    <App />
-  
+
+  <App />
+
 );
 
 
